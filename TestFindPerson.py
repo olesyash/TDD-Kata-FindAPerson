a8739f4f812ad4bcbf3b10518c3fdae3dc7520c2
olesyash <ilana.olesya@gmail.com>
2015-05-12 09:33:45 +0300

    1/3 Green, find or in list of posts

--- TestFindPerson.py
+++ TestFindPerson.py
@@ -3,10 +3,16 @@ from crowdmap import Crowdmap
 
 class FindPersonTests(unittest.TestCase):
     def setUp(self):
-        self.crowdmap = Crowdmap()
+        self.crowdmap = Crowdmap(["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley"])
 
     def test_getAllPostsForName(self):
-       posts = self.crowdmap.get_all_postsfor("Or")
-       self.assertIn("Or", posts)
+       posts = self.crowdmap.get_all_posts_for("Or")
+       self.assertTrue(posts)
 
+    def test_get_all_posts_for_missing_name(self):
+        posts = self.crowdmap.get_all_posts_for("Or2")
+        self.assertFalse(posts)
 
+    def test_homework_examlpe(self):
+        posts = self.crowdmap.get_all_posts_for("Or2")
+        self.assertIn("Or A", posts)
--- crowdmap.py
+++ crowdmap.py
@@ -1,5 +1,11 @@
 __author__ = 'olesya'
 
 class Crowdmap():
-    def get_all_postsfor(self, name):
-        return "aa"
+    def __init__(self, l_post):
+        self.posts_list = l_post
+
+    def get_all_posts_for(self, name):
+        for l in self.posts_list:
+            if name in l:
+                return True
+        return False
